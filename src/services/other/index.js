d5f8fbcc2ddc64769b84dd61c81f49f76db653c2
user.name: Work <user.email: renie@filotrack.com>
2022-11-10 13:15:07 +0100

    feat: TEST other api

--- src/services/other/index.js
+++ src/services/other/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/business-redirect', async (req, res, next) => {
     try {
-        res.redirect('https://filotrack.com/business')
+        res.send('<script>window.location.href="https://filotrack.com";</script>')
     } catch (error) {
         console.log(error);
         next(error);
