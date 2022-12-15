7c3d4a7b1b34890419d5b3c7810d6501f4d23e0b
user.name: Work <user.email: renie@filotrack.com>
2022-12-15 16:20:18 +0100

    feat: cors removed

--- index.js
+++ index.js
@@ -36,12 +36,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://filo-dashboard-2022.web.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.get('/', async (req, res, next) => {
     try {
         res.send("Express")
